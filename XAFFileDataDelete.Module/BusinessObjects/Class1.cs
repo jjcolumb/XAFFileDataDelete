282af3a535f4016c34bb92b7ed14ea3ece81d84b
Jose Javier Columbie <josejaviercolumbie@yahoo.com>
2023-12-13 15:31:33 -0700

    Add project files.

---
+++ XAFFileDataDelete.Module/BusinessObjects/Class1.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XAFFileDataDelete.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class PersistentClass : BaseObject
+    {
+        public PersistentClass(Session session) : base(session)
+        { }
+
+
+        FileData testFile;
+        public FileData TestFile
+        {
+            get => testFile;
+            set => SetPropertyValue(nameof(TestFile), ref testFile, value);
+        }
+    }
+}
